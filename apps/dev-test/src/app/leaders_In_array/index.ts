a4e859eb7879647c85ff789cd236cbed236a2860
Manuel Alejandro Cortes <alejandro.cortes@gklusa.com>
2022-09-20 11:50:54 -0400

    spanish rules

--- apps/dev-test/src/app/leaders_In_array/index.ts
+++ apps/dev-test/src/app/leaders_In_array/index.ts
@@ -3,6 +3,9 @@
 Given an array of positive integers. Your task is to find the leaders in the array.
 Note: An element of array is leader if it is greater than or equal to all the elements to its right side. Also, the rightmost element is always a leader.
 
+Dado un array de números positivos enteros. La tarea es encontrar los líderes del array
+Notas: Un elemento del array es líder si es mayor o igual que todos los elementos a su derecha. El elemento más a la derecha simpre es líder.
+
 */
 
 /* Test:
--- apps/dev-test/src/app/subarray-given-sum/index.ts
+++ apps/dev-test/src/app/subarray-given-sum/index.ts
@@ -2,6 +2,8 @@
 
 Given an unsorted array A of size N of non-negative integers, find a continuous sub-array which adds to a given number S.
 
+Dado un array no ordenado A de tamaño N de números interos no negativos, encontrar un sub-array contínuo que al sumarse sus elementos el resultado sea igual a un número S
+
 */
 
 /* Test:
